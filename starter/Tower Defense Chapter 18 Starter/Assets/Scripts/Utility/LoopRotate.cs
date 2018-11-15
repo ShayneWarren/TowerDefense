2660434c46e2ff7439d9843f6a3e819d7a7cd738
SLCTECH\swarren <littleone73_15@hotmail.com>
2018-11-15 11:15:43 -0500

    All progress of chapter 18 so far.

---
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Utility/LoopRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopRotate : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Vector3 rotation;
+
+    // Update is called once per frame
+    void Update ()
+    {
+        transform.Rotate(rotation * Time.deltaTime);
+    }
+}
