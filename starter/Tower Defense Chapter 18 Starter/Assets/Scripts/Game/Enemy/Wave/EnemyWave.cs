dd9653b8e40eb21c02247c7be3234563d7d64b86
SLCTECH\swarren <littleone73_15@hotmail.com>
2018-11-22 11:11:00 -0500

    completed chapter 18

---
+++ starter/Tower Defense Chapter 18 Starter/Assets/Scripts/Game/Enemy/Wave/EnemyWave.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+//1
+[Serializable]
+public class EnemyWave
+{
+    //2
+    public int pathIndex;
+    //3
+    public float startSpawnTimeInSeconds;
+    //4
+    public float timeBetweenSpawnsInSeconds = 1f;
+    //5
+    public List<GameObject> listOfEnemies = new List<GameObject>();
+}
